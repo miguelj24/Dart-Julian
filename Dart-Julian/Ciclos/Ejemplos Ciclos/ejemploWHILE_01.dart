de587c7615fa9b1abf0510d62e046b9c9440e3ff
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-05-07 11:57:19 -0500

    ejemplos WHILE 1,2 y 3

---
+++ Dart-Julian/Ciclos/Ejemplos Ciclos/ejemploWHILE_01.dart
@@ -0,0 +1,8 @@
+void main (List<String> args){
+  // Variable tipo CONTADOR
+  int contador = 0;
+  while(contador <=5 ){
+    print("Contador> $contador");
+    contador++; // Se incrementa de 1 en 1
+  }
+}
---
+++ Dart-Julian/Ciclos/Ejemplos Ciclos/ejemploWHILE_03.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  // Desarrolla un algoritmo que lea n numeros positivos,
+  // determine y muestre el promedio de los n numeros
+  // positivos
+}
